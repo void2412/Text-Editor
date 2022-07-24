a65df05b8e8f373de201a7095c5ebf2800b7e3db
void2412 <void2412@gmail.com>
2022-07-24 14:25:36 +0930

    implement asset caching

--- client/src-sw.js
+++ client/src-sw.js
@@ -4,7 +4,7 @@ const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
-
+const { StaleWhileRevalidate } = require('workbox-strategies')
 precacheAndRoute(self.__WB_MANIFEST);
 
 const pageCache = new CacheFirst({
@@ -27,4 +27,18 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+	({ request }) => request.destination === 'image',
+	new CacheFirst({
+	  cacheName: 'asset-cache',
+	  plugins: [
+		new CacheableResponsePlugin({
+		  statuses: [0, 200],
+		}),
+		new ExpirationPlugin({
+		  maxEntries: 60,
+		  maxAgeSeconds: 30 * 24 * 60 * 60,
+		}),
+	  ],
+	})
+  );
