bb6f907a8d1e1af3b8ed333bcbdc77a8df4689bf
void2412 <void2412@gmail.com>
2022-07-24 15:52:33 +0930

    changed back to production and remove todo comment

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,7 +12,6 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
 	try{
 		console.log('Adding content to the database');
@@ -34,7 +33,6 @@ export const putDb = async (content) => {
 	}
 };
 
-// TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
 	try{
 		console.log('Getting content from the database');
